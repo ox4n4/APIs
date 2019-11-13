ee8c9b21c61d86afc2fc33da039b0e8a2177033e
ox4n4 <oxana.pilenko@gmail.com>
2019-11-13 18:25:09 -0500

    newAPI

---
+++ apis/dhntest.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+headers = { 'Content-Type': 'application/json'}
+
+def get_team_stats():
+        #fetches from the best team
+        api_url = 'https://statsapi.web.nhl.com/api/v1/teams/8/stats'
+        response = requests.get(api_url, headers=headers)
+
+        if response.status_code == 200:
+                return json.loads(response.content.decode('utf-8'))
+        else:
+                return None
+
+team_info = get_team_stats()
+
+if team_info is not None:
+    print("Successfully created the .JSON file")
+    for team in team_info.items():
+        with open('habs_team_info.json', 'w') as f:
+                print(json.dumps(team, indent=2), file=f)
+
+else:
+        print('[!] Request Failed')
